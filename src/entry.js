e10b71a4a3d67eb3ebb9a8feec0c6b05dc61e1f6
manfred <manfred@adverway.com>
2017-02-07 17:38:51 +0100

    New config files and dependencies

--- src/entry.js
+++ src/entry.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-window.React = React;
-window.ReactDOM = ReactDOM;
+import TodoApp from './todo_app';
 
-import {TodoApp} from './todo-app';
-
-const todos = [
-    { id: 1, title: 'Comprar BF1', completed: true },
-    { id: 2, title: 'Arreglar TV', completed: false },
-    { id: 3, title: 'L4D2 party', completed: false }
-];
+import styles from './normalize.css';
 
 ReactDOM.render(
-    <TodoApp todos={todos}></TodoApp>,
-    document.getElementById('app'));
-
-/*<App counters={[
- * { title: "Victories", initialValue: 10 },
- * { title: "Defeats", initialValue: 20 },
- * { title: "Sucks", initialValue: 9000 }
- * ]} />,
- * */
+  React.createElement(TodoApp),
+  document.getElementById('app'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader', 'eslint-loader'] },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]']}
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader', 'eslint-loader'] },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[local]__[hash:base64:5]', 'postcss-loader'] }
     ]
-  }
+  },
 };

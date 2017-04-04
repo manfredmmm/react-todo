f3a5845189021732acf47ca1a032ca8815c4760d
manfred <manfredmiravitllasmas@gmail.com>
2017-04-04 14:25:25 +0200

    Refactoring todo app component

--- src/todo_app.jsx
+++ src/todo_app.jsx
@@ -1,72 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 
-import * as moment from 'moment';
-import TODOS from './todos';
-import STATUS from './status';
-
 import store from './store';
-
 import HeaderComponent from './header/header.component';
 import TodoForm from './todo/todo_form.component';
 import ChangeListFilter from './todo/change_list_filter.component';
 import TodoList from './todo/todo_list.component';
 import SearchTodo from './todo/search_todo.component';
 
-class TodoApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoId: 3,
-      data: TODOS,
-      filteredData: [],
-      status: STATUS.pending
-    };
-  }
-
-  _addNewTodo(value) {
-    const newTodo = {
-      id: this.state.todoId += 1,
-      name: value,
-      status: STATUS.pending,
-      description: '',
-      date: `${moment()}`
-    };
-    const newTodos = [
-      ...this.state.data,
-      newTodo
-    ];
-    this.setState({ data: newTodos });
-  }
-
-  _searchTodo(value) {
-    let filteredTodos;
-    if (!value) {
-      this.setState({ filteredData: '' });
-    } else {
-      filteredTodos = this.state.data
-          .filter(todo => todo.name.toLowerCase().includes(value.toLowerCase()));
-      this.setState({ filteredData: filteredTodos });
-    }
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <div>
-          <HeaderComponent />
-          <TodoForm
-            addTodo={value => this._addNewTodo(value)}
-          />
-          <SearchTodo
-            searchTodo={value => this._searchTodo(value)}
-          />
-          <TodoList />
-          <ChangeListFilter />
-        </div>
-      </Provider>
-    );
-  }
-}
+const TodoApp = () => (
+  <Provider store={store}>
+    <div>
+      <HeaderComponent />
+      <SearchTodo />
+      <TodoForm />
+      <TodoList />
+      <ChangeListFilter />
+    </div>
+  </Provider>
+);
 
 export default TodoApp;

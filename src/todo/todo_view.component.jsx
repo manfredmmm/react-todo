d9e9100c077710179bb787abc11066f6ec07bab7
manfred <manfredmiravitllasmas@gmail.com>
2017-04-18 13:51:47 +0200

    Todo page styles

--- src/todo/todo_view.component.jsx
+++ src/todo/todo_view.component.jsx
@@ -1,14 +1,22 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from './todo_view.css';
+
 const TodoView = ({ todo }) => (
-  <div>
-    <h1>ID: {todo.id}</h1>
+  <div className={styles.body}>
+    <h1 className={styles.title}>ID: {todo.id}</h1>
     <h2>Name: {todo.name}</h2>
     <p>Description: {todo.description}</p>
     <p>Status: {todo.status}</p>
     <p>Date: {todo.date}</p>
-    <Link to="/">Go back</Link>
+    <Link
+      to="/"
+      className={styles.link}
+    >
+      <i className="fa fa-chevron-left" />
+      <span>Go back</span>
+    </Link>
   </div>
 );
 
